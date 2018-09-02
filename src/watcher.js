874eaab8048f201e8a58906f4a1cc70e5e3dbbd4
Muthu Kumaran <emailiamkumaran@gmail.com>
2018-09-02 17:51:05 +0530

    first commit

---
+++ src/watcher.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const log = require('./log.js')
+const mm = require('micromatch')
+const chalk = require('chalk')
+const watcher = require('simple-watcher')
+
+class Watcher {
+  watch (workingDir, exclude, callback) {
+    log.info(`Scanning: ${chalk.yellow(workingDir)} ...`)
+
+    watcher(workingDir, (localPath) => {
+      log.debug('Changed:', localPath)
+
+      // Skip excluded.
+      if (exclude && mm([localPath], exclude).length > 0) {
+        return
+      }
+
+      callback(localPath)
+    }, 0)
+
+    log.info('Awaiting changes ...')
+  }
+}
+
+module.exports = Watcher
